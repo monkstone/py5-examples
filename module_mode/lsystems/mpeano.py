429e32dfca3356ae27c8550763ef8ecf5147a423
monkstone <mamba2928@yahoo.co.uk>
2021-09-15 19:59:42 +0100

    refactored examples

--- module_mode/lsystems/mpeano.py
+++ module_mode/lsystems/mpeano.py
@@ -1,3 +1,7 @@
+import py5
+from math import cos, pi, sin, sqrt, radians
+from grammar import grammar
+from collections import namedtuple
 
 """
 mpeano.py by Martin Prout
@@ -5,42 +9,52 @@ LSystem rules from The Euclidean Traveling Salesman Problem.... by MG Norman & P
 Features a scaling adjustment and turtle reversing, use trignometry rather than processing affine
 transforms to calculate the line, uses a grammar module to create production string.
 """
-import py5
-from math import cos, pi, sin, sqrt
-from grammar import grammar
 
-# some globals
-XPOS = 0
-YPOS = 1
-ANGLE = 2
-DELTA = py5.PI / 4
+w = 600
+h = 600
 
-RULES = {
-'F' : '',
-'Y': 'FFY',
-'X' : '+!X!FF-BQFI-!X!FF+',
-'A' : 'BQFI',
-'B' : 'AFF'
-}
+LSystem = namedtuple('MPeano', 'start, rules, turn_angle_deg')
 
-AXIOM = 'XFF2-AFF2-XFF2-AFF'
+mpeano = LSystem(
+        start = 'XFF2-AFF2-XFF2-AFF',
+        rules = dict(
+            F = '',
+            Y = 'FFY',
+            X = '+!X!FF-BQFI-!X!FF+',
+            A = 'BQFI',
+            B = 'AFF'
+            ),
+        turn_angle_deg = 45
+    )
+
+def settings():
+    py5.size(w, h)
+
+def setup():
+    sketch_title('MPeano')
+
+    production = grammar.generate(mpeano, 6)
+    py5.background(0, 0, 255)
+    py5.stroke(255, 255, 0)
+    py5.stroke_weight(3)
+    render(production)
 
 def render(production):
     """
     Render evaluates the production string and calls draw_line
     """
-    delta = DELTA
+    delta = radians(mpeano.turn_angle_deg)
     distance = 15
-    turtle = [py5.width/10, py5.height/10, -delta]
+    turtle = {'x': w / 10, 'y': h / 10, 'angle': -delta}
     repeat = 1
     for val in production:
         if val == "F":
             turtle = draw_line(turtle, distance)
         elif val == "+":
-            turtle[ANGLE] += delta * repeat
+            turtle['angle'] += delta * repeat
             repeat = 1
         elif val == "-":
-            turtle[ANGLE] -= delta * repeat
+            turtle['angle']-= delta * repeat
             repeat = 1
         elif val == "I":
           distance *= 1/sqrt(2)
@@ -53,29 +67,15 @@ def render(production):
         else:
             pass
 
-
 def draw_line(turtle, length):
     """
     Draw line utility uses processing 'line' function to draw lines
     """
-    turtlecopy = []
-    turtlecopy[:] = turtle
-    turtlecopy[XPOS] = turtle[XPOS] + length * cos(turtle[ANGLE])
-    turtlecopy[YPOS] = turtle[YPOS] - length * sin(turtle[ANGLE])
-    py5.line(turtle[XPOS], turtle[YPOS], turtlecopy[XPOS], turtlecopy[YPOS])
+    turtlecopy = turtle.copy()
+    turtlecopy['x'] = turtle['x'] + length * cos(turtle['angle'])
+    turtlecopy['y'] = turtle['y'] - length * sin(turtle['angle'])
+    py5.line(turtle['x'], turtle['y'], turtlecopy['x'], turtlecopy['y'])
     return turtlecopy
-
-def settings():
-    py5.size(600, 600)
-
-def setup():
-    sketch_title('MPeano')
-
-    production = grammar.repeat(6, AXIOM, RULES)
-    py5.background(0, 0, 255)
-    py5.stroke(255, 255, 0)
-    py5.stroke_weight(3)
-    render(production)
 
 def sketch_title(title):
     py5.get_surface().set_title(title)
